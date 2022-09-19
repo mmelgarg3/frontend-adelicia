fdd8914689c4e092ca59677c6f904ea716b775f2
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-09-19 12:50:16 -0600

    remove password confirmation from register page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,109 +1,123 @@
-import React, { useState } from 'react'
-import axios from "axios";
-import { useHistory } from "react-router-dom";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const Register = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [surname, setSurname] = useState('');
-    const [confPassword, setConfPassword] = useState('');
-    const [msg, setMsg] = useState('');
-    const history = useHistory();
-    const [role, setRole] = useState('');
+	const [ name, setName ] = useState('');
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
+	const [ surname, setSurname ] = useState('');
+	const [ msg, setMsg ] = useState('');
+	const history = useHistory();
+	const [ role, setRole ] = useState('');
 
+	const handleChange = (event) => {
+		var value = event.target.value;
+		console.log(value);
+		setRole(value);
+	};
 
+	const Register = async (e) => {
+		e.preventDefault();
+		try {
+			await axios.post('http://localhost:5000/users', {
+				name: name,
+				surname: surname,
+				email: email,
+				password: password,
+				role: role
+			});
+			history.push('/');
+		} catch (error) {
+			if (error.response) {
+				setMsg(error.response.data.msg);
+			}
+		}
+	};
 
-    const handleChange =(event)=>{
-      var value = event.target.value;
-      console.log(value);
-      setRole(value);
-    }
+	return (
+		<section className="hero has-background-grey-light is-fullheight is-fullwidth">
+			<div className="hero-body">
+				<div className="container">
+					<div className="columns is-centered">
+						<div className="column is-4-desktop">
+							<form onSubmit={Register} className="box">
+								<p className="has-text-centered">{msg}</p>
+								<div className="field mt-5">
+									<label className="label">Name</label>
+									<div className="controls">
+										<input
+											type="text"
+											className="input"
+											placeholder="Name"
+											value={name}
+											onChange={(e) => setName(e.target.value)}
+										/>
+									</div>
+								</div>
+								<div className="field mt-5">
+									<label className="label">Surname</label>
+									<div className="controls">
+										<input
+											type="text"
+											className="input"
+											placeholder="Surname"
+											value={surname}
+											onChange={(e) => setSurname(e.target.value)}
+										/>
+									</div>
+								</div>
+								<div className="field mt-5">
+									<label className="label">Email</label>
+									<div className="controls">
+										<input
+											type="text"
+											className="input"
+											placeholder="Email"
+											value={email}
+											onChange={(e) => setEmail(e.target.value)}
+										/>
+									</div>
+								</div>
+								<div className="field mt-5">
+									<label className="label">Password</label>
+									<div className="controls">
+										<input
+											type="password"
+											className="input"
+											placeholder="******"
+											value={password}
+											onChange={(e) => setPassword(e.target.value)}
+										/>
+									</div>
+								</div>
+								<div className="field mt-5">
+									<label className="label">Confirm Password</label>
+									<div className="controls">
+										<select
+											className="form-select"
+											aria-label="Default select"
+											onChange={handleChange}
+										>
+											<option>Seleccione el rol</option>
+											<option value="1">Administrador</option>
+											<option value="2">Cocinero</option>
+											<option value="3">Mesero</option>
+											<option value="4">Cliente</option>
+										</select>
+									</div>
+								</div>
 
-    const Register = async (e) => {
-        e.preventDefault();
-        try {
-            await axios.post('http://localhost:5000/users', {
-                name: name,
-                surname: surname,
-                email: email,
-                password: password,
-                confPassword: confPassword,
-		        role: role
-            });
-            history.push("/");
-        } catch (error) {
-            if (error.response) {
-                setMsg(error.response.data.msg);
-            }
-        }
-    }
+								<div className="field mt-5">
+									<button className="button is-success is-fullwidth">Register</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
 
-    return (
-        <section className="hero has-background-grey-light is-fullheight is-fullwidth">
-            <div className="hero-body">
-                <div className="container">
-                    <div className="columns is-centered">
-                        <div className="column is-4-desktop">
-                            <form onSubmit={Register} className="box">
-                                <p className="has-text-centered">{msg}</p>
-                                <div className="field mt-5">
-                                    <label className="label">Name</label>
-                                    <div className="controls">
-                                        <input type="text" className="input" placeholder="Name"
-                                            value={name} onChange={(e) => setName(e.target.value)} />
-                                    </div>
-                                </div>
-                                <div className="field mt-5">
-                                    <label className="label">Surname</label>
-                                    <div className="controls">
-                                        <input type="text" className="input" placeholder="Surname"
-                                            value={surname} onChange={(e) => setSurname(e.target.value)} />
-                                    </div>
-                                </div>
-                                <div className="field mt-5">
-                                    <label className="label">Email</label>
-                                    <div className="controls">
-                                        <input type="text" className="input" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                                    </div>
-                                </div>
-                                <div className="field mt-5">
-                                    <label className="label">Password</label>
-                                    <div className="controls">
-                                        <input type="password" className="input" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} />
-                                    </div>
-                                </div>
-                                <div className="field mt-5">
-                                    <label className="label">Confirm Password</label>
-                                    <div className="controls">
-                                        <input type="password" className="input" placeholder="******" value={confPassword} onChange={(e) => setConfPassword(e.target.value)} />
-                                    </div>
-                                </div>
-				<div className="field mt-5">
-                                    <label className="label">Confirm Password</label>
-                                    <div className="controls">
-				      <select className="form-select" aria-label="Default select"
-				      onChange={handleChange}>
-					<option >Seleccione el rol</option>
-					<option value="1">Administrador</option>
-					<option value="2">Cocinero</option>
-					<option value="3">Mesero</option>
-					<option value="4">Cliente</option>
-				      </select>
-
-                                    </div>
-                                </div>
-
-                                <div className="field mt-5">
-                                    <button className="button is-success is-fullwidth">Register</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
-}
-
-export default Register
+export default Register;
