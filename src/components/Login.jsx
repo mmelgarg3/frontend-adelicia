3179a7d496e571a9fb1c3892818891e817f84150
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-09-20 15:55:21 -0600

    migrating Login page to bootstrap

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -34,28 +34,27 @@ const Login = () => {
   }
 
     return (
-        <section className="hero has-background-grey-light is-fullheight is-fullwidth">
-            <div className="hero-body">
-                <div className="container">
-                    <div className="columns is-centered">
-                        <div className="column is-4-desktop">
+      <section className="container" style={{marginTop: 100}}>
+            <div className="row">
+                    <div className="col-md-6 mx-auto">
+                        <div className="card-body">
                             <form onSubmit={Auth} className="box">
                                 <p className="has-text-centered">{msg}</p>
-                                <div className="field mt-5">
+                                <div className="form-group">
                                     <label className="label">Email</label>
                                     <div className="controls">
-                                        <input type="text" className="input" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                        <input type="text" className="form-control" placeholder="example@gmail.com" value={email} onChange={(e) => setEmail(e.target.value)} />
                                     </div>
                                 </div>
-                                <div className="field mt-5">
+                                <div className="from-group">
                                     <label className="label">Password</label>
                                     <div className="controls">
-                                        <input type="password" className="input" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} />
+                                        <input type="password" className="form-control" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} />
                                     </div>
                                 </div>
-				<div className="field mt-5">
-                                    <label className="label">Rol</label>
-                                    <div className="controls">
+				<div className="form-group">
+                                    <label className="label mt-4">Rol</label>
+                                    <div className="">
 				      <select className="form-select" aria-label="Default select"
 				      onChange={handleChange}>
 					<option >Seleccione el rol</option>
@@ -66,13 +65,12 @@ const Login = () => {
 				      </select>
                                     </div>
                                 </div>
-                                <div className="field mt-5">
-                                    <button className="button is-success is-fullwidth">Login</button>
+                                <div className="form-group">
+                                    <button className="btn btn-success btn-block">Login</button>
                                 </div>
                             </form>
                         </div>
                     </div>
-                </div>
             </div>
         </section>
     )
