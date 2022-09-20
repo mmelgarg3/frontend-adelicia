cf0ad5a4f0bf7b2099160e129aa760f0f78f80c7
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-09-20 17:21:38 -0600

    fixing errors with bootstrap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,23 +16,23 @@ const Navbar = () => {
     
     return (
       <>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	  <a class="navbar-brand" href="#">Adelicia's Restaurant</a>
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-	    <span class="navbar-toggler-icon"></span>
+	<nav className="navbar navbar-expand-lg navbar-light bg-light">
+	  <a className="navbar-brand" href="#">Adelicia's Restaurant</a>
+	  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span className="navbar-toggler-icon"></span>
 	  </button>
 
-	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-	    <ul class="navbar-nav mr-auto">
-	      <li class="nav-item active">
-		<a class="nav-link" href="#">Catalogo<span class="sr-only">(current)</span></a>
+	  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul className="navbar-nav mr-auto">
+	      <li className="nav-item active">
+		<a className="nav-link" href="#">Catalogo<span className="sr-only">(current)</span></a>
 	      </li>
-	      <li class="nav-item">
-		<a class="nav-link" href="#">Pedidos</a>
+	      <li className="nav-item">
+		<a className="nav-link" href="#">Pedidos</a>
 	      </li>
 	    </ul>
-	    <form class="form-inline my-2 my-lg-0">
-	      <button class="btn btn-outline-danger my-2 my-sm-0" type="button" onClick={Logout}>Logout</button>
+	    <form className="form-inline my-2 my-lg-0">
+	      <button className="btn btn-outline-danger my-2 my-sm-0" type="button" onClick={Logout}>Logout</button>
 	    </form>
 	  </div>
 	</nav>
