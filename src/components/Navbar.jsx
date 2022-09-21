5d6183f342ca35736ac761d58fb48e8abbb9a5b4
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-09-20 18:50:27 -0600

    reafactoring code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
     return (
       <>
 	<nav className="navbar navbar-expand-lg navbar-light bg-light">
-	  <a className="navbar-brand" href="#">Adelicia's Restaurant</a>
+	  <a className="navbar-brand" href="/">Adelicia's Restaurant</a>
 	  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span className="navbar-toggler-icon"></span>
 	  </button>
@@ -25,10 +25,10 @@ const Navbar = () => {
 	  <div className="collapse navbar-collapse" id="navbarSupportedContent">
 	    <ul className="navbar-nav mr-auto">
 	      <li className="nav-item active">
-		<a className="nav-link" href="#">Catalogo<span className="sr-only">(current)</span></a>
+		<a className="nav-link" href="/Products">Catalogo<span className="sr-only">(current)</span></a>
 	      </li>
 	      <li className="nav-item">
-		<a className="nav-link" href="#">Pedidos</a>
+		<a className="nav-link" href="/Pedidos">Pedidos</a>
 	      </li>
 	    </ul>
 	    <form className="form-inline my-2 my-lg-0">
