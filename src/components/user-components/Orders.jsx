795d23b9377040b9d2569724363e7ecd46ac1675
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-09-20 22:06:29 -0600

    showing Orders from local storage

--- src/components/user-components/Orders.jsx
+++ src/components/user-components/Orders.jsx
@@ -1,6 +1,19 @@
+import {useState, useEffect} from 'react';
+
+
 export default function Orders(){
 
+  const [products, setProducts] = useState([]);
 
+  useEffect(() =>{
+    getData();
+  });
+
+  const getData = ()=>{
+    const data = window.localStorage.getItem('orders');
+    if(data !== null) setProducts(JSON.parse(data));
+
+  }
 
   const handleClick = (e)=>{
     e.preventDefault();
@@ -11,6 +24,16 @@ export default function Orders(){
 
   return(
     <>
+      <div className="container">
+	{products.map((prd) => (
+	    <div className="card" key={prd.id}>
+	      <p>{prd.nombre}</p>
+	      <p>{prd.descripcion}</p>
+	      <p>{prd.precio}</p>
+	    </div>
+	  ))}
+	
+      </div>
       <button className="btn btn-danger" onClick={handleClick}> Confirmar Pedido </button>
     </>
   );
