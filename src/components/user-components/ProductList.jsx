dbb5ede935559ebe1b5f117305d600811926b4d8
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-09-21 09:09:34 -0600

    fixing memory leak when render component

--- src/components/user-components/ProductList.jsx
+++ src/components/user-components/ProductList.jsx
@@ -16,7 +16,7 @@ const ProductList = () => {
     useEffect(() => {
         refreshToken();
 	getProducts();
-    });
+    }, []);
 
     const refreshToken = async () => {
         try {
@@ -88,27 +88,30 @@ const ProductList = () => {
 		<b>{name} {surname}</b>
 	    </h4>
 
-	    <div className="card-deck" 
+	    <div className="row row-cols-1 row-cols-md-3" 
 	      style={{marginTop: 100, marginBottom: 20}}>
 		      {products.map((prd, index) => (
-			<div className="card" key={prd.id} 
-			style={{width:18 + "rem"}}>
-			<img src={prd.imagen} 
-			style={{height: 200, objectFit: 'cover'}} 
-			className="card-img-top" alt="..." />
-			  <div className="card-body">
-			    <h5 className="card-title">{prd.nombre}</h5>
-			    <p className="card-text text-justify">
-			      {prd.descripcion}
-			    </p>
-			    <h3 >Q.{prd.precio}</h3>
-			    <button onClick={event => handleAdd(event,prd)}
-			      className="btn btn-warning btn-block">
-				Agregar  
-			      <i className="fas fa-cart-shopping"></i> 
-			    </button>
+			<div className="col mb-4">
+			  <div className="card" key={prd.id} 
+			  style={{width:18 + "rem"}}>
+			  <img src={prd.imagen} 
+			  style={{height: 200, objectFit: 'cover'}} 
+			  className="card-img-top" alt="..." />
+			    <div className="card-body">
+			      <h5 className="card-title">{prd.nombre}</h5>
+			      <p className="card-text text-justify">
+				{prd.descripcion}
+			      </p>
+			      <h3 >Q.{prd.precio}</h3>
+			      <button onClick={event => handleAdd(event,prd)}
+				className="btn btn-warning btn-block">
+				  Agregar  
+				<i className="fas fa-cart-shopping"></i> 
+			      </button>
+			    </div>
 			  </div>
 			</div>
+			
 		      ))}
 	      </div>
         </div>
