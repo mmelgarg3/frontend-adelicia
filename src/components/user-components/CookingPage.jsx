999739a7e522ea3d1843d945c1d21afa19c5bfc6
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-09-27 22:12:20 -0600

    new changes

--- src/components/user-components/CookingPage.jsx
+++ src/components/user-components/CookingPage.jsx
@@ -1,7 +1,108 @@
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+
 export default function CookingPage(){
+  const [token, setToken] = useState('');
+  const [expire, setExpire] = useState('');
+  const [orders, setOrders] = useState([]);
+  const history = useHistory();
+
+  useEffect(()=>{
+    getOrders();
+    refreshToken();
+  }, []);
+
+  const refreshToken = async () => {
+      try {
+	  const response = await axios.get('http://localhost:5000/token');
+	  setToken(response.data.accessToken);
+	  const decoded = jwt_decode(response.data.accessToken);
+	  setExpire(decoded.exp);
+      } catch (error) {
+	  if (error.response) {
+	      history.push("/");
+	  }
+      }
+  }
+
+  const axiosJWT = axios.create();
+
+  axiosJWT.interceptors.request.use(async (config) => {
+      const currentDate = new Date();
+      if (expire * 1000 < currentDate.getTime()) {
+	  const response = await axios.get('http://localhost:5000/token');
+	  config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+	  setToken(response.data.accessToken);
+	  const decoded = jwt_decode(response.data.accessToken);
+	  setExpire(decoded.exp);
+      }
+      return config;
+  }, (error) => {
+      return Promise.reject(error);
+  });
+
+  const getOrders = async () => {
+      const response = await axios.get('http://localhost:5000/orders', {
+	params: {
+	  estado: 1
+	},
+      });
+      console.log(response.data);
+      setOrders(response.data);
+  }
+
+  const removeOrder = (id)=>{
+    const new_arr = orders.filter(el => el.id == id)
+    setOrders(new_arr);
+  }
+
+  const handleClick = async( id)=>{
+    try{
+      await axios.post('http://localhost:5000/change-status',{
+	id: id
+      });
+      removeOrder(id);
+    }catch(err){
+      console.log(err);
+    }
+  }
   return(
-    <>
-      <h2>coocking page</h2>
-    </>
-  ) ;
+      <>
+	<h2 className="card-title ml-4 mt-4 text-primary">Ordenes</h2>
+	<div className="container" style={{marginTop: 80}}>
+	  <div className="row">
+	    <div className="col-md-8 mx-auto">
+	      <table className="table">
+		<thead className="thead-dark">
+		  <tr>
+		    <th scope="col">#</th>
+		    <th scope="col">fecha</th>
+		    <th scope="col">Usuario</th>
+		    <th scope="col">Estado</th>
+		    <th scope="col">Total</th>
+		    <th scope="col">Acciones</th>
+		  </tr>
+		</thead>
+		<tbody>
+		  {orders.map((ord, index) => (
+		    <tr key={ord.id}>
+		      <th scope="row">{index}</th>
+		      <td>{ord.fecha}</td>
+		      <td>{ord.idUsuario}</td>
+		      <td>{ord.estado}</td>
+		      <td>Q.{ord.totalPedido}</td>
+		      <td>
+			<button onClick={(e) => handleClick(ord.id)} className="btn btn-danger">Cocinar</button>
+		      </td>
+		    </tr>
+		  ))}
+		</tbody>
+	      </table>
+	    </div>
+	  </div>
+	</div>
+      </>
+  );
 }
