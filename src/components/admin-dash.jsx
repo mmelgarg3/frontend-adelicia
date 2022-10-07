46f15dd720ea790a8c08946f1b222c9ed6d7a365
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-10-07 07:33:35 -0600

    reading data from database2 clear

--- src/components/admin-dash.jsx
+++ src/components/admin-dash.jsx
@@ -1,6 +1,26 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 
 const AdminDashboard = () => {
+
+  const getData = async()=>{
+    try{
+      const response = await axios.get("http://localhost:5000/all-info");
+      console.log(response);
+    }catch(err){
+      console.log(err);
+    }
+
+  }
+
+  useEffect(()=>{
+    getData();
+  });
+
+
+
+
   return(
     <>
       <div className="container">
