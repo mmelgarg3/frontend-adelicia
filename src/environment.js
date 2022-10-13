31e246299c9eb78c0f8b79ffda4a43307afc1e78
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-10-12 23:06:39 -0600

    changing route of api

--- src/environment.js
+++ src/environment.js
@@ -1,3 +1,4 @@
-export const api_route = "http://localhost:5000";
+// export const api_route = "http://localhost:5000";
+export const api_route = "https://adelicias-backend-app.azurewebsites.net/";
 
 
