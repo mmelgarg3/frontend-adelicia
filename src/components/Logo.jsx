bad99135a1c58e2ac4682c88d587dd5ecd131e25
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-10-14 13:50:03 -0600

    cambiando tamaño de logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,6 @@ export default function Logo(){
   return(
     <div>
 	<img id="logo" src="https://github.com/Olivers11/Images/blob/master/adlicias_background-removebg-preview.png?raw=true" alt=""/>
-  );
     </div>
   );
 
